3644afc0bd74713482204489d738bb4d81b689dd
Tom Callahan <tcallaha@gmail.com>
2016-04-15 17:45:08 -0500

    added callback fn

--- nature-sdl.lisp
+++ nature-sdl.lisp
@@ -1,3 +1,4 @@
+(ql:quickload :ltk)
 (ql:quickload :lispbuilder-sdl)
 
 (defpackage :nature
@@ -5,9 +6,24 @@
 
 (in-package :nature)
 
-(load "/home/tcallahan/bottega/nature/nature-ltk.lisp")
+;(load "/home/tcallahan/bottega/nature/nature-ltk.lisp")
 
-(defun setup ()
+(defun update-label (label text)
+  (setf (text label) text))
+
+(defun main ()
+  (with-ltk ()
+    (let* ((frame (make-instance 'frame :width 80 :height 140))
+	   (button (make-instance 'button :master frame :text "Start"))
+	   (label (make-instance 'label :master frame))
+	   (label-fn (lambda (evt) (update-label label evt))))
+      (setf (command button) (lambda () (setup label-fn)))
+      (pack frame)
+      (pack label)
+      (pack button)
+      (process-events))))
+
+(defun setup (fn)
   (sdl:with-init ()
     (sdl:window 800 600 :title-caption "fun coding nature")
     (setf (sdl:frame-rate) 30)
@@ -17,6 +33,8 @@
       (:key-down-event (:key key)
 		       (if (sdl:key= key :sdl-key-escape) (sdl:push-quit-event)))
       (:idle ()
+	     (when (sdl:mouse-left-p)
+	       (funcall fn (sdl:mouse-x)))
 	     (update-display)
 	     (process-events)))))
 
