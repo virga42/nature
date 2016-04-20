62b8ef7aa72170cd7eca189f41450bf76d103dca
virga42 <tcallaha@gmail.com>
2016-04-20 14:55:31 -0400

    switched to array

--- random-distribution.lisp
+++ random-distribution.lisp
@@ -3,21 +3,21 @@
 (in-package :nature)
 
 (defparameter *size* 10)
-(defparameter *random-set* (make-list *size* :initial-element 0))
+(defparameter *random-set* (make-array *size* :initial-element 0))
 (defparameter *counter* 0)
 
 (defun setup ()
-  (setf *random-set* (make-list (length *random-set*) :initial-element 0))
+  (setf *random-set* (fill *random-set* 0))
   (setf *counter* 0))  
 
 (defun draw (&optional (n 5))
-  (when (< *counter* 100)
+  (when (< *counter* 1000)
     (dotimes (i n)
-      (setf *random-set* (update-nth (random-number-generator) *random-set*))
+      (incf (elt *random-set* (random-number-generator))))
       ;; (print *random-set*)
       (clear-display *black*)
       (render-bars *random-set*))
-    (setf *counter* (1+ *counter*))))
+    (setf *counter* (1+ *counter*)))
 
 (defun random-number-generator ()
   (random *size*))
@@ -27,14 +27,16 @@
 ;;   (loop for i from 0 for j in list collect (if (= i n) (1+ j) j)))
 
 ;;; update-nth recursive-style
-(defun update-nth (n list)
-  (if (> n 0)
-      (cons (car list) (update-nth (1- n) (cdr list)))
-      (cons (1+ (car list)) (cdr list))))
+;; (defun update-nth (n list)
+;;   (if (> n 0)
+;;       (cons (car list) (update-nth (1- n) (cdr list)))
+;;       (cons (1+ (car list)) (cdr list))))
 
 (defun render-bars (set)
-  (loop for i from 0 for j in set do
-       (draw-rectangle-* (* i 10) 0 10 j :color *blue*)))
+  (let* ((width (/ *window-width* *size*))
+	 (random-set-list (coerce *random-set* 'list)))
+  (loop for i from 0 for j in random-set-list do
+       (draw-rectangle-* (* i width) (- *window-height* j) width j :color *blue*))))
 
 (defun standard-deviation (list)
   (let* ((n (length list))
@@ -48,3 +50,10 @@
 			    (mapcar #'(lambda (x) (expt (- x mean) 2))
 				    list))
 		    n)))))))
+
+;; Generate 12 numbers from 0->1
+;; Sum them
+;; Subtract 6
+
+(defun random-gauss ()
+  )
